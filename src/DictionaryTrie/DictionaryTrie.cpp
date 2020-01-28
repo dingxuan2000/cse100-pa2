cc557c1bd5cf282a5a353a02d912f9b7281ee86d
Alice-Ding <dingxuan0112@gmail.com>
2020-01-28 16:56:57 +0000

    Check insert().

--- src/DictionaryTrie/DictionaryTrie.cpp
+++ src/DictionaryTrie/DictionaryTrie.cpp
@@ -19,41 +19,31 @@ bool DictionaryTrie::insert(string word, unsigned int freq) {
     //如果当前node不是end of word, 那么copy of freq就是0, 否则, cfreq是freq的值
     int i = 0;
     int cfreq;
-    if (i != word.length() - 1)
-        cfreq = 0;
-    else
-        cfreq = freq;
+    // if (i != word.length() - 1)
+    //     cfreq = 0;
+    // else
+    //     cfreq = freq;
     //我们需要Insert整个word, 所以当超过了word的length的话，就结束insert
+    TSTNode* curr;
+    curr = this->root;
     while (i < word.length()) {
         // case1: empty trie, insert the whole string as the only one word in
-        // the trie
-        TSTNode* curr;
-        curr = this->root;
+        // the trie, 如果current node是空的话，就create a new node.
         if (curr == NULL) {
+            if (i != word.length() - 1)
+                cfreq = 0;
+            else
+                cfreq = freq;
             curr = new TSTNode(word[i], cfreq);
-            //这里也是要如何recursivly create new node for each middle child?
-            curr->middle = insertChar(word[i++], cfreq);
         } else {
             //当这个trie不是空的，首先比较root的char和word的第一个char:
             //如果想要insert word的第一个char比root大的话，去找root右边的孩子
-            if (curr->getLetter() < word[i]) {
+            if (curr->getLetter() < word[i])
                 curr = curr->right;
-                /**1. if curr->right == null, create a new node, and set this
-                 *new node as the curr's right child, Then for the rest of the
-                 *char, recursively insert under the curr's right child.
-                 *2. if curr->right != null, 那么继续traverse the tree, curr =
-                 *curr->right;
-                 */
 
-            }
-            // curr = insertChar(word[i], cfreq);
-            else if (word[i] < curr->getLetter()) {
-                /**这里的情况和上面一样，需要添加
-                 */
+            else if (word[i] < curr->getLetter())
                 curr = curr->left;
-            } else {
-                /**需要完善
-                 */
+            else {
                 // curr = insertChar(word[i], cfreq);
                 curr = curr->middle;
                 i++;
@@ -130,7 +120,7 @@ DictionaryTrie::~DictionaryTrie() {}
 //     newNode->left = newNode->middle = newNode->right = NULL;
 //     return newNode;
 // }
-TSTNode* insertChar(char letter, int freq) {
-    TSTNode* node = new TSTNode(letter, freq);
-    return node;
-}
+// TSTNode* insertChar(char letter, int freq) {
+//     TSTNode* node = new TSTNode(letter, freq);
+//     return node;
+// }
